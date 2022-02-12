698cff45c30165370a76743ed8e6e55aafc86980
Andrew Boltachev <andrewboltachev@gmail.com>
2022-02-12 05:46:26 +0300

    check git

--- mega-copy.py
+++ mega-copy.py
@@ -1,9 +1,13 @@
 import sys
 from fs_utils import _run
+from termcolor import cprint
 
 
 print("hello", sys.argv)
 
-a=_run(r'git status --porcelain', 3)
-print(a[0], a[1], len(a[1]))
 
+if __name__ == "__main__":
+    a = _run(r'git status --porcelain', 3)
+    if len(a[1]):
+        cprint("Error: Git isn\'t clean, can\'t perform work\n", "red")
+        sys.exit(1)
